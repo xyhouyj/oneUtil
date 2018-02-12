a81fd1b8bd28bd5bf3ca021a6882b6bee3982ba8
hyj <1046271658@qq.com>
2018-02-12 18:01:19 +0800

    测试类

--- src/main/java/com/oneUtil/vos/Person.java
+++ src/main/java/com/oneUtil/vos/Person.java
@@ -1,5 +1,7 @@
 package com.oneUtil.vos;
 
+import java.math.BigDecimal;
+
 /**
  * Created by houyunjuan on 2018/2/12.
  */
@@ -8,9 +10,96 @@ public class Person {
     private String name;
 
     private int age;
-
-
     private String homeAddress;
+
+    private BigDecimal amount;
+
+    private Long score;
+
+    private double money;
+
+    private float totalPrice;
+
+    private boolean flag;
+
+    private byte b;
+
+    private char c;
+
+    private short number;
+
+    public Person() {
+        System.out.println("无参构造方法。。。。。。");
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Long getScore() {
+        return score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public byte getB() {
+        return b;
+    }
+
+    public void setB(byte b) {
+        this.b = b;
+    }
+
+    public char getC() {
+        return c;
+    }
+
+    public void setC(char c) {
+        this.c = c;
+    }
+
+    public short getNumber() {
+        return number;
+    }
+
+    public void setNumber(short number) {
+        this.number = number;
+    }
 
     public String getHomeAddress() {
         return homeAddress;
