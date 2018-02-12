a0b170393a50591c1442df5a66eee212b414baac
hyj <1046271658@qq.com>
2018-02-12 13:26:18 +0800

    测试类

---
+++ src/main/java/com/oneUtil/tests/TestOne.java
@@ -0,0 +1,10 @@
+package com.oneUtil.tests;
+
+/**
+ * Created by houyunjuan on 2018/2/12.
+ */
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
